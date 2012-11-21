da3944a1bcbef1b01b52cb859f7ad3771d5e73df
Jan Buschtöns <buschtoens@gmail.com>
2012-11-21 18:03:36 +0100

    Fix typo in test case

--- test/cubixcraft.de.js
+++ test/cubixcraft.de.js
@@ -2,7 +2,7 @@
 var sanitize = require("../");
 
 // Tests
-describe("stats('cubixcraft.de')", fuction() {
+describe("stats('cubixcraft.de')", function() {
 	stats("cubixcraft.de").on("stats", function(data) {
 		data.should.have.keys("motd", "onlinePlayers", "maxPlayers", "latency");
 	});
